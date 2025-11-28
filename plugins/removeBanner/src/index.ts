6762cb900439e5f1460022e0bbbc0a47e8b9687c
vaiskiainen <vainonkeri@gmail.com>
2025-11-28 20:02:16 +0200

    faah

--- plugins/removeBanner/src/index.ts
+++ plugins/removeBanner/src/index.ts
@@ -1,32 +1,60 @@
-import { metro } from "@vendetta/metro";
-import { patcher } from "@vendetta/patcher";
+import { findByProps, findByDisplayName } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+import { instead } from "@vendetta/patcher"; 
 
-let unpatch: () => void = () => {};
-
-
-const GuildHeaderModule = metro.find(m => m.default?.displayName === "GuildHeader");
+let patches: Function[] = [];
 
 export default {
   onLoad() {
 
-    if (!GuildHeaderModule) {
-      console.error("[RemoveBanner] Didn't find GuildHeaderModel!");
-      return; 
+    const Header = findByDisplayName("GuildHeader", false) || 
+                  findByProps("GuildHeader")?.GuildHeader;
+
+    if (!Header) {
+      console.warn("[RemoveBanner] GuildHeader not found");
+      return;
     }
 
-    unpatch = patcher.before("default", GuildHeaderModule, (args) => {
-      const [props] = args;
-      
-      if (props) {
-        props.banner = null;
-        props.bannerImage = null;
-        props.bannerSource = null;
-      }
-    });
+
+    patches.push(
+      after("render", Header.prototype, (_, ret) => {
+        if (ret?.props?.header) {
+
+          if (ret.props.header.props?.bannerSource) {
+            ret.props.header.props.bannerSource = null;
+          }
+          if (ret.props.header.props?.banner) {
+            ret.props.header.props.banner = null;
+          }
+        }
+
+
+        if (ret?.props?.banner) ret.props.banner = null;
+        if (ret?.props?.bannerSource) ret.props.bannerSource = null;
+        if (ret?.props?.guild?.banner) ret.props.guild.banner = null;
+
+        return ret;
+      })
+    );
+
+
+    const GuildStore = findByProps("getGuild");
+    if (GuildStore) {
+      patches.push(
+        instead("getGuild", GuildStore, (args, orig) => {
+          const guild = orig(...args);
+          if (guild) guild.banner = null;
+          return guild;
+        })
+      );
+    }
+
+    console.log("[RemoveBanner] Loaded successfully – banners hidden!");
   },
 
   onUnload() {
-    unpatch();
+    patches.forEach(p => p());
+    patches = [];
   },
 
   settings: null
