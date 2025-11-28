fe3eaa0535c769f94a974aa7fab8ef508bee5ef4
vaiskiainen <vainonkeri@gmail.com>
2025-11-28 19:54:17 +0200

    test

--- plugins/removeBanner/src/index.ts
+++ plugins/removeBanner/src/index.ts
@@ -1,38 +1,33 @@
 import { metro } from "@vendetta/metro";
 import { patcher } from "@vendetta/patcher";
 
-
 let unpatch: () => void = () => {};
+
 
 const GuildHeaderModule = metro.find(m => m.default?.displayName === "GuildHeader");
 
 export default {
   onLoad() {
 
-    if (!GuildHeaderModule) return;
-
+    if (!GuildHeaderModule) {
+      console.error("[RemoveBanner] Didn't find GuildHeaderModel!");
+      return; 
+    }
 
     unpatch = patcher.before("default", GuildHeaderModule, (args) => {
-
       const [props] = args;
       
-
-
       if (props) {
         props.banner = null;
         props.bannerImage = null;
         props.bannerSource = null;
       }
-      
-
     });
   },
 
   onUnload() {
-
     unpatch();
   },
-
 
   settings: null
 };
