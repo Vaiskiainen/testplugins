9aeb0dad503f41bfea83020d7311d26d6cd5944e
Vaiskiainen <164906395+Vaiskiainen@users.noreply.github.com>
2025-12-01 18:39:32 +0200

    made the code use after()

--- plugins/removeBanner/src/index.ts
+++ plugins/removeBanner/src/index.ts
@@ -1,4 +1,4 @@
-import { instead } from "@vendetta/patcher";
+import { after } from "@vendetta/patcher";
 import { findByProps, findByStoreName, findByDisplayName } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
 import SettingsPanel from "./Settings";
@@ -6,69 +6,88 @@ import SettingsPanel from "./Settings";
 storage.removeBanner ??= true;
 storage.removeSplash ??= false;
 storage.aggressiveMode ??= true;
-storage.whitelist ??= []; 
+storage.whitelist ??= [];
 
-let patches: Function[] = [];
+let patches = [];
 
 export default {
   onLoad() {
-    const unload = () => patches.forEach(p => p && p());
+    const unload = () => patches.forEach(p => p?.());
     const load = () => {
       unload();
       patches = [];
       [findByProps("getGuild"), findByStoreName("GuildCacheStore"), findByStoreName("GuildStore")]
         .filter(Boolean)
         .forEach(store => {
-          store.getGuild && patches.push(
-            instead("getGuild", store, (args, orig) => {
-              const g = orig(...args);
-              if (g && storage.removeBanner && !storage.whitelist.includes(g.id)) {
-                g.banner = g.bannerId = null;
-              }
-              if (g && storage.removeSplash && !storage.whitelist.includes(g.id)) {
-                g.splash = null;
-              }
-              return g;
-            })
-          );
+          if (!store.getGuild) return;
+
+          patches.push(after("getGuild", store, (args, res) => {
+            if (!res) return res;
+
+            const id = res.id;
+
+            if (storage.removeBanner && !storage.whitelist.includes(id)) {
+              res.banner = null;
+              res.bannerId = null;
+            }
+
+            if (storage.removeSplash && !storage.whitelist.includes(id)) {
+              res.splash = null;
+            }
+
+            return res;
+          }));
         });
+
       [findByProps("getGuildBannerURL"), findByProps("getGuildSplashURL")]
         .filter(Boolean)
         .forEach(mod => {
-          if (storage.removeBanner && mod.getGuildBannerURL) {
-            patches.push(instead("getGuildBannerURL", mod, (args, orig) => {
+          if (mod.getGuildBannerURL) {
+            patches.push(after("getGuildBannerURL", mod, (args, url) => {
               const guild = args[0];
-              if (guild && storage.whitelist.includes(guild.id)) return orig(...args);
+              if (!storage.removeBanner) return url;
+              if (guild && storage.whitelist.includes(guild.id)) return url;
               return null;
             }));
           }
-          if (storage.removeSplash && mod.getGuildSplashURL) {
-            patches.push(instead("getGuildSplashURL", mod, (args, orig) => {
+
+          if (mod.getGuildSplashURL) {
+            patches.push(after("getGuildSplashURL", mod, (args, url) => {
               const guild = args[0];
-              if (guild && storage.whitelist.includes(guild.id)) return orig(...args);
+              if (!storage.removeSplash) return url;
+              if (guild && storage.whitelist.includes(guild.id)) return url;
               return null;
             }));
           }
         });
+
+
       if (storage.aggressiveMode) {
-        const Header = findByProps("GuildHeader")?.GuildHeader ||
+        const Header =
+          findByProps("GuildHeader")?.GuildHeader ||
           findByDisplayName("GuildHeader", false);
-        Header?.prototype?.render && patches.push(
-          instead("render", Header.prototype, (args, orig) => {
-            const res = orig(...args);
-            if (res?.props && res.props.guild && !storage.whitelist.includes(res.props.guild.id)) {
-              res.props.banner = res.props.bannerSource = null;
-              res.props.guild.banner = null;
-            }
+
+        if (Header?.prototype?.render) {
+          patches.push(after("render", Header.prototype, (args, res) => {
+            const guild = res?.props?.guild;
+            if (!guild || storage.whitelist.includes(guild.id)) return res;
+
+            res.props.banner = null;
+            res.props.bannerSource = null;
+            guild.banner = null;
+
             return res;
-          })
-        );
+          }));
+        }
       }
     };
+
     load();
   },
+
   onUnload() {
-    patches.forEach(p => p && p());
+    patches.forEach(p => p?.());
   },
-  settings: SettingsPanel
+
+  settings: SettingsPanel,
 };
