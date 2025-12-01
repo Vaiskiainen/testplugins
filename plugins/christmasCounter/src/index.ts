762c4f42b8d6dceafc8314e7fb1ee0c810f31bb9
vaiskiainen <vainonkeri@gmail.com>
2025-12-01 16:07:06 +0200

    added a christmas counter plugin

---
+++ plugins/christmasCounter/src/index.ts
@@ -0,0 +1,27 @@
+import { storage } from "@vendetta/plugin";
+import { showToast } from "@vendetta/ui/toasts";
+
+const getDaysToChristmas = () => {
+  const now = new Date();
+  let christmas = new Date(now.getFullYear(), 11, 25);
+  if (now > christmas) {
+    christmas.setFullYear(now.getFullYear() + 1);
+  }
+
+  const difference = christmas.getTime() - now.getTime();
+  return Math.floor(difference / (1000 * 60 * 60 * 24));
+};
+
+
+export default {
+  onLoad() {
+    const now = new Date();
+    const currentDate = now.toISOString().slice(0, 10);
+
+    if (storage.lastShown !== currentDate) {
+      const days = getDaysToChristmas();
+      showToast(`Only ${days} days until Christmas!`);
+      storage.lastShown = currentDate;
+    }
+  },
+};
