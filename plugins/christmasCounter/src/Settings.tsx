fab5c23b6dc16d0666ef4a31ea6a1849bc34b7b7
Vaiskiainen <164906395+Vaiskiainen@users.noreply.github.com>
2025-12-04 21:18:38 +0200

    Update Settings.tsx

--- plugins/christmasCounter/src/Settings.tsx
+++ plugins/christmasCounter/src/Settings.tsx
@@ -2,25 +2,22 @@ import { React } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
 
-const { FormRow, FormSwitch } = Forms;
+const { FormSwitchRow } = Forms;
 
 export default function Settings() {
   const [use25th, setUse25th] = React.useState(storage.christmasDay === 25);
 
   return (
     <>
-      <FormRow
+      <FormSwitchRow
         label="Count towards December 25"
-        subLabel="Disable this to count towards December 24 instead."
-      >
-        <FormSwitch
-          value={use25th}
-          onValueChange={(v: boolean) => {
-            setUse25th(v);
-            storage.christmasDay = v ? 25 : 24;
-          }}
-        />
-      </FormRow>
+        subLabel="Disable to count towards December 24 instead."
+        value={use25th}
+        onValueChange={(v: boolean) => {
+          setUse25th(v);
+          storage.christmasDay = v ? 25 : 24;
+        }}
+      />
     </>
   );
 }
