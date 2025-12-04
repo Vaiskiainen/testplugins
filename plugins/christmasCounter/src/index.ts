8a67c0db4c8d3a3c0011a027ea5af7995724535f
Vaiskiainen <164906395+Vaiskiainen@users.noreply.github.com>
2025-12-04 14:31:01 +0200

    Update index.ts

--- plugins/christmasCounter/src/index.ts
+++ plugins/christmasCounter/src/index.ts
@@ -1,9 +1,17 @@
 import { storage } from "@vendetta/plugin";
 import { showToast } from "@vendetta/ui/toasts";
+import Settings from "./Settings";
+
+storage.lastShown ??= null;
+storage.christmasDay ??= 24; 
 
 const getDaysToChristmas = () => {
   const now = new Date();
-  let christmas = new Date(now.getFullYear(), 11, 25);
+
+  
+  const targetDay = storage.christmasDay;
+
+  let christmas = new Date(now.getFullYear(), 11, targetDay);
   if (now > christmas) {
     christmas.setFullYear(now.getFullYear() + 1);
   }
@@ -14,7 +22,6 @@ const getDaysToChristmas = () => {
 
 export default {
   onLoad() {
-    
     setTimeout(() => {
       const now = new Date();
       const currentDate = now.toISOString().slice(0, 10);
@@ -24,6 +31,8 @@ export default {
         showToast(`Only ${days} days until Christmas! 🎁`);
         storage.lastShown = currentDate;
       }
-    }, 1200); // 1.2 seconds ensures UI is ready
+    }, 1200);
   },
+
+  settings: Settings,
 };
