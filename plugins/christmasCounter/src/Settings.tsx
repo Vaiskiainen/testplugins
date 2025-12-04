0a59091e8a1aca619088919538e91ce06b853ddc
Vaiskiainen <164906395+Vaiskiainen@users.noreply.github.com>
2025-12-04 20:46:48 +0200

    Refactor Settings component and update labels

--- plugins/christmasCounter/src/Settings.tsx
+++ plugins/christmasCounter/src/Settings.tsx
@@ -1,27 +1,26 @@
-import { React } from "@vendetta/metro";
+import { React } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { Forms } from "@vendetta/ui/components";
 
 const { FormRow, FormSwitch } = Forms;
 
-export default () => {
+export default function Settings() {
   const [use25th, setUse25th] = React.useState(storage.christmasDay === 25);
 
   return (
     <>
       <FormRow
-        label="Count towards the 25th"
-        subLabel="Enable to count Christmas as December 25 instead of 24."
-        trailing={
-          <FormSwitch
-            value={use25th}
-            onValueChange={(v) => {
-              setUse25th(v);
-              storage.christmasDay = v ? 25 : 24;
-            }}
-          />
-        }
-      />
+        label="Count towards December 25"
+        subLabel="Disable this to count towards December 24 instead."
+      >
+        <FormSwitch
+          value={use25th}
+          onValueChange={(v: boolean) => {
+            setUse25th(v);
+            storage.christmasDay = v ? 25 : 24;
+          }}
+        />
+      </FormRow>
     </>
   );
-};
+}
